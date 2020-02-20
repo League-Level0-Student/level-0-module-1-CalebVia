73dca5a7a29a35b0fac23f15e6daa11b2ad30e07
CalebVia <55600946+CalebVia@users.noreply.github.com>
2020-02-20 15:53:30 -0800

    Finished Modual 1

---
+++ src/_12_badgers/Badger.java
@@ -0,0 +1,80 @@
+package _12_badgers;
+
+import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class Badger {
+
+public static void main(String[] args) {
+	
+for (int i = 0; i < 28; i++) {
+	if (i <=11 || (i>=13&&i<=24)) {
+	System.out.println("badger");
+		
+}
+	
+	if (i ==12) {
+		System.out.println("mushroom");
+	}
+
+	if (i >=24) {
+		System.out.println("SNAKE");
+	}
+	
+
+
+
+
+
+
+
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}	
+}
